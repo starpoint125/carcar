4ebd98ccb5472783d5132860db7d7229adb15e5b
starpoint125 <starpoint125@126.com>
2023-11-07 13:52:18 +0800

    dev

--- common/config/params.php
+++ common/config/params.php
@@ -1,9 +1,9 @@
 <?php
 return [
-    'supportEmail' => 'admin@feehi.com',
+    'supportEmail' => 'admin@admin.com',
     'user.passwordResetTokenExpire' => 3600,
     'site' => [
-        'url' => 'http://cms.feehi.com',//此配置用来正确的在前台显示后台上传的文件，会被后台 设置->网站设置 网站域名覆盖
+        'url' => 'http://car.cso123.com',//此配置用来正确的在前台显示后台上传的文件，会被后台 设置->网站设置 网站域名覆盖
         'sign' => '###~SITEURL~###',//数据库中保存的本站地址，展示时替换成正确url
     ],
     'category.template.directory' => [//分类列表页模版
@@ -15,4 +15,6 @@ return [
     'page.template.directory' => [//单页模版
         Yii::getAlias("@frontend/views/page/view.php") => "默认",
     ],
+    //佣金
+    'commission' => 100,
 ];
